08b707300328589814ac48b5fca6473b5c6bbd54
Meet Jamsutkar <jamsutkarmeetpradeep@gmail.com>
2024-01-09 00:02:07 +0530

    django+react setup

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-
+@api_view(['GET'])
 def index(request):
-    return HttpResponse('Site setup')
+    return Response({'message': 'Hello, world!'})
 
 # Create your views here.
