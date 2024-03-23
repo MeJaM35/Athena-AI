57cd3f7f8d3194114394ab365a1da2869b9e80aa
Meet Jamsutkar <jamsutkarmeetpradeep@gmail.com>
2024-03-23 14:41:05 +0530

    Models: added brand & ig table
    Instruction:
    1. makemigration
    2. migrate

--- core/models.py
+++ core/models.py
@@ -13,3 +13,21 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
 
     REQUIRED_FIELDS = ['username']
+
+class Brand(models.Model):
+    name = models.CharField(max_length=256, blank=True, null=True)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    address = models.TextField(blank=True, null=True)
+    size = models.IntegerField(default=-1)  # -1 means not
+
+
+class Instagram(models.Model):
+    brand = models.OneToOneField(Brand, on_delete=models.CASCADE)
+    url = models.URLField()
+    tag = models.CharField(max_length=256, blank=True, null=True)
+    totalposts = models.IntegerField()
+    totalfollows = models.IntegerField()
+    tenthlikes = models.IntegerField()
+    eleventhlikes = models.IntegerField()
+    twelthlikes = models.IntegerField()
+    lastninereach = models.IntegerField()
